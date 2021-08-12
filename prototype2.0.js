943f574a362a0cc910d2f9d757aeaac8c6a56654
Mari L <lesteberg@gmail.com>
2021-08-12 10:28:04 +0200

    fixing scratching sound

--- prototype2.0.js
+++ prototype2.0.js
@@ -213,7 +213,7 @@ function updateFieldIfNotNull(fieldName, value, precision=2){
     ///////////////////////////////////////////////
 
     // Scaling values for inverted volume-control
-    var fn = generateScaleFunction(0.3, 3, 0.9, 0);
+    var fn = generateScaleFunction(0.3, 3, 0.9, 0.1);
     newAcc = fn(totAcc);
     newAcc = (clamp(0, 0.9, newAcc));
     let tempo = Math.floor(newAcc * 150);
