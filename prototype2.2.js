25c0c6ddee995bbc262cf29fa7eb49194fc6cfb2
fractionMari <lesteberg@gmail.com>
2021-10-12 12:24:51 +0200

    improving web-app based on feedback from users. 4. tuning audio parameters

--- prototype2.2.js
+++ prototype2.2.js
@@ -39,8 +39,8 @@ reverb.dampening = 1000;
 
 reverb.wet.value = 0.2;
 //const autoWah = new Tone.AutoWah(100, 5, -10).connect(reverb);
-autoWah.Q.value = 9;
-autoWah.wet.value = 0.2;
+//autoWah.Q.value = 9;
+//autoWah.wet.value = 0.2;
 let buttonOn = false;
 let buttonOn2 = false;
 let buttonOn3 = false;
@@ -635,7 +635,7 @@ function updateFieldIfNotNull(fieldName, value, precision=2){
 
 
 
-    autoWah.baseFrequency = yDotValues;
+    //autoWah.baseFrequency = yDotValues;
     pingPong.delayTime = xDotValues / 100;
    // autoWah.octaves = (xDotValues / 20) + 5;
 
